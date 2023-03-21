e3fadcfa722af0f7d5268cf6a86e8c9a266a2158
Haruhiro <takenokonomura0303@gmail.com>
2023-03-21 10:01:59 +0900

    nake exe file

---
+++ src/main.py
@@ -0,0 +1,37 @@
+from tkinter import messagebox
+import sys
+import pyperclip
+import re
+import deepl
+import json
+
+## initialize api
+# read api key
+with open("api_key.json", "r") as file:
+    dict = json.load(file)
+API_KEY = dict["key"]
+
+# check api key
+if API_KEY == "your_api_key":
+    messagebox.showerror('Error', 'Enter your deepl key in [api_key.json]')
+    sys.exit()
+
+# initialize
+translator = deepl.Translator(API_KEY)
+
+##param
+source_lang = 'EN'
+target_lang = 'JA'
+
+## main
+# get english text
+text_raw = pyperclip.paste()
+
+# remove paragraph
+text = re.sub("\n", " ", text_raw)
+
+# execute translate
+result = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
+
+# copy clipbord
+pyperclip.copy(result.text)
